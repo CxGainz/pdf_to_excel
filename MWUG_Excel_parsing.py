1c036f68dd8753d4183a9ee44005b66059492578
cxGains <connorg553@gmail.com>
2023-05-20 00:32:22 -0700

    modify excel parsing file

--- MWUG_Excel_parsing.py
+++ MWUG_Excel_parsing.py
@@ -1,3 +1,12 @@
 """
 Excel parsing
+For when the title element didn't parse correctly, look where a word has a Capital letter in the middle of it,(
+if capital letter TRUE in middle of word, then split from there, that is where the title begins.
 """
+import xlwings as xw
+
+wb = xw.Book('mwug_test_file.xlsx')
+
+sheet = wb.sheets['Sheet1']
+sheet['A1'].value = 'Foo1'
+
